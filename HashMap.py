ce992b94ae614e4e7a8b86f4e78f20f01ad0b2e2
code-0000 <hashirbinabdulazeez@gmail.com>
2022-12-01 10:55:35 +0530

    second commit

--- HashMap.py
+++ HashMap.py
@@ -1,7 +1,7 @@
 class HashMap:
     def __init__(self) -> None:
         self.MAX = 10
-        self.arr = [[] for i in range(self.MAX)]
+        self.arr = [None for i in range(self.MAX)]
 
     def hash(self, key):
         h = 0
@@ -13,17 +13,33 @@ class HashMap:
 
     def __setitem__(self, key, value):
         h = self.hash(key)
-        for idx, element in enumerate(self.arr[h]):
-            if element[0] == key:
-                self.arr[h][idx] = (key, value)
+        for i in range(h, self.MAX):
+            if self.arr[i] == None:
+                self.arr[i] = (key, value)
                 return
-        self.arr[h].append((key, value))
+            elif self.arr[i][0] == key:
+                self.arr[i] = (key, value)
+                return
+        for i in range(0, h):
+            if self.arr[i] == None:
+                self.arr[i] = (key, value)
+                return
+            elif self.arr[i][0] == key:
+                self.arr[i] = (key, value)
+                return
+        print('the Map is full')
+            
 
     def __getitem__(self, key):
         h = self.hash(key)
-        for element in (self.arr[h]):
-            if element[0] == key:
-                return element[1]
+        for i in range(h, self.MAX):
+            if self.arr[h][0] == key:
+                return self.arr[h][1]
+        for i in range(0, h):
+            if self.arr[h][0] == key:
+                return self.arr[1]
+        print('Item is not in the Map')
+
 
     def __delitem__(self, key):
         h = self.hash(key)
@@ -35,4 +51,14 @@ class HashMap:
 
 if __name__ == '__main__':
     t = HashMap()
-        
+    t['feb 1'] = 'saturday'
+    t['feb 2'] = 'sundaty'
+    t['feb 3'] = 'monday'
+    t['feb 4'] = 'tuesday'
+    t['feb 5'] = 'wendsday'
+    t['feb 6'] = 'thursday'
+    t['feb 7'] = 'friday'
+    t['feb 8'] = 'saturday'
+    t['feb 9'] = 'sunday'
+    print(t.arr)
+    print(t['feb 9'])
